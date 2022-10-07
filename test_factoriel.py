185fadbf1c8ffc59096c8f061e6b3b11d5a8ea43
Layesekou <sekou.kaba.thridchild@gmail.com>
2022-10-07 09:35:43 +0100

    TF

---
+++ test_factoriel.py
@@ -0,0 +1,13 @@
+import unittest 
+from factoriel import Math
+
+class Test_factoriel(unittest.TestCase):
+    def setUp(self):
+        self.p = Math(4)
+    def test_parametre_vide(self):
+        self.assertIsNotNone(self.p.n,msg="Entrez le paramètre de la fonction")
+    def test_instance(self):
+        self.assertNotIsInstance(self.p.n,str)
+
+if __name__ =="__main__":
+    unittest.main()
